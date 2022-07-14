07c04ea66f040a9fd856b351c37c89ad83ad8f05
guybenda <guybenda@users.noreply.github.com>
2022-07-15 02:14:20 +0300

    x

---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public GameObject from;
+    public GameObject to;
+    public GameObject enemy;
+    public GameObject container;
+    public float delay = 0.3f;
+    public int maxEnemies = 30;
+
+    private float currentDelay = 0f;
+    private Transform[] waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDelay = delay;
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint").Select(g => g.transform).ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        currentDelay -= Time.fixedDeltaTime;
+
+        if (currentDelay <= 0f)
+        {
+            currentDelay += delay;
+            if (container.transform.childCount < maxEnemies) CreateEnemy();
+        }
+    }
+
+    void CreateEnemy()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") == null) return;
+        var position = new Vector3(
+            Random.Range(from.transform.position.x, to.transform.position.x),
+            0f,
+            Random.Range(from.transform.position.z, to.transform.position.z)
+        );
+
+        position.y = Terrain.activeTerrain.SampleHeight(position);
+        Debug.Log(position.y);
+        enemy = Instantiate(enemy, position, Quaternion.identity, container.transform);
+
+        enemy.GetComponent<EnemyScript>().waypoints = waypoints;
+    }
+}
